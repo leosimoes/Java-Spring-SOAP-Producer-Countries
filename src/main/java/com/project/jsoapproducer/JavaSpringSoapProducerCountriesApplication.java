1b12e4ff1cdf299e33635dc8657520b6904f5bc7
Leonardo <leosimoes.engcomp@gmail.com>
2024-04-26 14:25:23 -0300

    Project started

---
+++ src/main/java/com/project/jsoapproducer/JavaSpringSoapProducerCountriesApplication.java
@@ -0,0 +1,13 @@
+package com.project.jsoapproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringSoapProducerCountriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringSoapProducerCountriesApplication.class, args);
+	}
+
+}
